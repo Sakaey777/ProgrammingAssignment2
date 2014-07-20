146b31fe81a591b86a71bf859b84a23c9fa60d0f
Sakaey777 <sakaey777@gmail.com>
2014-07-20 21:23:30 +0900

    Add my code

--- cachematrix.R
+++ cachematrix.R
@@ -5,13 +5,49 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
-#test commit
+    # cache clear
+    m.cache <- NULL
 
+    # set function
+    set <- function(y) {
+        x.cache <<- y
+        m.cache <<- NULL
+    }
+
+    # set matrix data
+    get <- function() x
+
+    # set cache data
+    setmtx <- function(mtxi){
+        m.cache <<- mtxi
+        message("Setting cache data")
+    }
+
+    # get cache data
+    getmtx <- function() m.cache
+        
+    # Return object as list
+    list(set = set, get = get, setmtx = setmtx, getmtx = getmtx)    
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+
+    ## If cashe can be used, use it!
+    ca <- x$getmtx()    
+    if(!is.null(ca)) {
+        message("getting cache data")
+        return(ca)
+    }
+    
+    ## Get matrix data and inverse... It's heavy.
+    message("Inversing now")
+    data <- x$get()
+    mtxi <- t(data)
+    x$setmtx(mtxi)
+
+    mtxi
 }
